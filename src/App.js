6bf1232ff303836ae7a865a8c527ecda501b07c3
DraciVik <viktor.stojanov90@gmail.com>
2021-04-26 08:51:08 +0200

    Finished with project configuration

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import React from "react";
 import ReactDOM from 'react-dom';
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Sudo",
-      animal: "Dog",
-      breed: "Wheaten Terrier",
-    }),
-  ]);
-};
+import Pet from "./Pet";
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
+
+
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
